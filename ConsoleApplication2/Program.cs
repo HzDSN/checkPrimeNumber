e64818bb24f5398be40fd0762a9cd967046a772e
dongs <dongs@DESKTOP-8R6AIJ0>
2016-11-29 22:14:13 +0800

    添加项目文件。

---
+++ ConsoleApplication2/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int i;
+            for (i = 2; i <= 100; i++)
+                if (checkPrimeNumber(i)) Console.WriteLine(i);
+        }
+
+        private static bool checkPrimeNumber(int i)
+        {
+            int x, f = 0;
+            for (x = 2; x <= i - 1; x++)
+            {
+                if (i % x == 0) f = 1;
+            }
+            if (f == 0)
+            { return true; }
+            else
+            { return false; }
+        }
+
+    }
+}
